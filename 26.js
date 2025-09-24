0ac8c7cf16f5c8da54449b4be7d44304e6636e07
Aksh Usrathe <akshusrathe007@gmail.com>
2025-09-24 23:25:53 +0530

    update_js

---
+++ 26.js
@@ -0,0 +1,24 @@
+//intro arrays
+//reference type
+
+//how to create
+//ordered collection of items
+
+let fruits = ["apple","mango","banana"];
+console.log(fruits);
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+let numbers = [1, 2, 3.4 , "kabir",true , null, undefined];
+console.log(numbers);
+
+console.log(fruits);
+fruits[0] = "orange";
+console.log(fruits);
+
+
+
---
+++ 27.js
@@ -0,0 +1,21 @@
+// array push pop
+
+// array shift unshift
+
+
+let fruits = ["apple","mango","banana"];
+//mutable object of array means we can change the array
+console.log(fruits);
+console.log(fruits.push("grapes")); //at the end
+console.log(fruits);
+console.log(fruits.pop()); //at the end
+console.log(fruits);
+console.log("``````````````````");
+console.log(fruits.unshift("grapes")); //at the start
+console.log(fruits);
+console.log(fruits.shift()); //at the start
+console.log(fruits);
+
+
+
+
