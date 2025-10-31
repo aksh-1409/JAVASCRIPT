1c1ff9b08c9048d7394dbbbfc9ea3f77898e29d0
Aksh Usrathe <akshusrathe007@gmail.com>
2025-10-31 20:02:41 +0530

    update_js

---
+++ 66.js
@@ -0,0 +1,29 @@
+//constructor function bolte hai apn createUser ko 
+function createUser(firstName, age, email, address){
+    this.firstName = firstName;
+    this.age = age;
+    this.email = email;
+    this.address = address;
+}
+createUser.prototype.about = function(){
+    console.log(this.firstName + " is " + this.age + " years old");
+}
+createUser.prototype.is18 = function(){
+    return this.age >= 18;
+}
+createUser.prototype.sing = function(){
+    return "la la la";
+}
+
+const user1 = new createUser("hasrshit", 22, "hasrshit@gmail.com", "345, home nagar");
+// console.log(user1);
+// user1.about();
+// console.log(user1.is18());
+// console.log(user1.sing());
+
+for(let key in user1){
+    // console.log(key); //print all keys including prototype keys
+    if  (user1.hasOwnProperty(key)){ // dont want to print prototype keys then 
+        console.log(key);
+    }
+}
