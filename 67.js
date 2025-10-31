95a8e3d9a406ffd418410384e0b16a0a8aa28f17
Aksh Usrathe <akshusrathe007@gmail.com>
2025-10-31 20:02:57 +0530

    update_js

---
+++ 67.js
@@ -0,0 +1,13 @@
+// let numbers = new Array(1,2,3,4,5);
+// console.log(numbers);
+// console.log(numbers.__proto__);
+// console.log(Array.prototype);
+// console.log(Object.getPrototypeOf(numbers));
+
+//same new ke saath jo hota hai vese hi constant literals ke saath bhi
+// let numbers =  [1,2,3,4,5];
+// console.log(numbers);
+// console.log(numbers.__proto__);
+// console.log(Array.prototype);
+// console.log(Object.getPrototypeOf(numbers));
+
