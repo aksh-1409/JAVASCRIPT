bb11f40aea87f8ad3a4d0c443e6a4144ac75e3fd
Aksh Usrathe <akshusrathe007@gmail.com>
2025-11-05 00:22:15 +0530

    update.js

---
+++ 74.js
@@ -0,0 +1,50 @@
+//static methods
+// class Person{
+//     constructor(firstName, lastName, age){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//     }
+
+//     static description(){
+//         return "This is a static method";
+//     }
+
+//     static desc = "This is a static property";
+
+//     get fullName(){
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+//     set fullName(fullName){
+//         const [firstName, lastName] = fullName.split(" ");
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+
+//     eat(){
+//         return `${this.firstName} is eating`;
+//     }
+
+//     isSuperCute(){
+//         return this.age <= 1;
+//     }
+
+//     isCute(){
+//         return true;
+//     }
+
+//     static description(){
+//         return "This is a static method";
+//     }
+// }
+
+// const person1 = new Person("hasrshit", "chauhan", 22);
+// console.log(Person.description());
+// console.log(person1.desc); 
+// console.log(Person.desc);
+
+
+
+console.log(typeof firstName);
+var firstName = "hasrshit";
+// console.log(firstName);
