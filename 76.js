2ac68a91f811ffcd211383291bb40143d230fb97
Aksh Usrathe <akshusrathe007@gmail.com>
2025-11-05 00:23:36 +0530

    update.js

---
+++ 76.js
@@ -0,0 +1,25 @@
+//to get element throgh class
+//getElementsbyClassName()
+//return html collection
+//array like object--
+//indexing
+//length property
+//lopping --
+//classical loop
+//for of loop
+//forEach loop not there because it is not array
+//but we can apply foreach method through converting the html collection into array
+//const htmlcollection = Array.from(html collection);
+//htmlcollection.forEach(function)
+
+
+
+//getqueryselectorAll()
+//return node list
+//array like object--
+//indexing
+//length property
+//lopping --
+//classical loop
+//for of loop
+//forEach loop 
