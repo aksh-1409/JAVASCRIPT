8a41d3b0a0fea2d769ab0f89329ba60fb640520e
Aksh Usrathe <akshusrathe007@gmail.com>
2025-11-05 17:11:41 +0530

    update.js

---
+++ 80.js
@@ -0,0 +1,8 @@
+//before and after
+
+// const oldUl  = document.querySelector("ul");
+// const ul = document.createElement("ul");
+// ul.innerText    = "new ul";
+// console.log(oldUl);
+// oldUl.before(ul);
+// oldUl.after(ul);
