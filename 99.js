1057d685cdf91e9fc48b6b7eda71883812a9e938
Aksh Usrathe <akshusrathe007@gmail.com>
2025-11-05 22:58:52 +0530

    Add 99.js

---
+++ 99.js
@@ -0,0 +1,11 @@
+export class Person{
+    constructor(firstname,lastname,age){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+
+    }
+    getFullName(){
+        console.log(`${this.firstname} ${this.lastname} ${this.age}`);
+    }
+}
