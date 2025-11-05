883b163fe483e5f8686b76530df2fcf52bdff61c
Aksh Usrathe <akshusrathe007@gmail.com>
2025-11-05 17:16:38 +0530

    Add 84.js

---
+++ 84.js
@@ -0,0 +1,5 @@
+//event object
+//browser jab bhi event hota hai to 2 kaam karta hai
+//1. event object create karta hai
+//2. callback function hai vo js engine ko degha(parameter me jo fucntion hai voh jo us event ke trigger hone pe run hogha yaa yeh bole ki event handler function hai)
+//3. event object ko event handler function ke andr as a argument pass karke deta hai
