709e6e16bdabdf345e2febd3bd197a07ddd0b7cb
Aksh Usrathe <akshusrathe007@gmail.com>
2025-11-05 17:16:09 +0530

    Add 81.js

---
+++ 81.js
@@ -0,0 +1,8 @@
+//static list //live list
+//nodelist //htmlcollection
+//querSelectorAll //getelementsbyclassName , getelementsbytagname , getElementsbysomething
+
+//so when we hold the NodeList(static list) and then again add or remove the element then it will not be updated
+//but when we hold the htmlcollection(live list) and then again add or remove the element then it will be updated
+
+//so when we want to update the list then we should use htmlcollection
